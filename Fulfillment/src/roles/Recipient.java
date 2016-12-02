ca507a30ed7c91bfcc2153c5a09b6a337e7f5370
fvescio <francesco.d.vescio@gmail.com>
2016-12-01 23:16:45 -0800

    Implement DeliverSpeed, Person, and Recipient

--- Fulfillment/src/roles/Recipient.java
+++ Fulfillment/src/roles/Recipient.java
@@ -1,5 +1,27 @@
 package roles;
 
-public class Recipient extends Person {
+import factories.ITelephone;
 
+public class Recipient{
+	private Person person;
+	private ITelephone contactNo;
+	public Recipient(Person person, ITelephone contactNo) {
+		super();
+		this.person = person;
+		this.contactNo = contactNo;
+	}
+	public Person getPerson() {
+		return person;
+	}
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	public ITelephone getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(ITelephone contactNo) {
+		this.contactNo = contactNo;
+	}
+	
+	
 }
