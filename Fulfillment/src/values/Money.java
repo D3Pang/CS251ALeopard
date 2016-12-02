100839fb1332a358b05cb04a21a8242743ffa618
Vikram Deshmukh <srvikram13@gmail.com>
2016-12-01 23:59:44 -0800

    Money completed!

--- Fulfillment/src/values/Money.java
+++ Fulfillment/src/values/Money.java
@@ -14,9 +14,24 @@ public class Money implements Comparable<Money> {
 	public String toString() {
 		return "Money["+Double.toString(amount)+" "+currency.toString()+"]";
 	}
-	
-	public int compareTo(Money o) {
-		// TODO Auto-generated method stub
+	public Money addTax(float tax) {
+		return new Money(amount+(amount*tax)/100, currency);
+	}
+	public Money add(Money other) {
+		return new Money(other.getAmount() + amount, currency);
+	}
+	public Money subtract(Money other) {
+		return new Money(other.getAmount() - amount, currency);
+	}
+	public Money multiply(double factor) {
+		return new Money(amount * factor, currency);
+	}
+	public Money divide(double factor) {
+		return new Money(amount * factor, currency);
+	}
+	public int compareTo(Money other) {
+		if(this.amount < other.getAmount()) return -1;
+		if(this.amount > other.getAmount()) return 1;
 		return 0;
 	}
 	
