b54ac559646bf0a9c5b69bfa38678c05dcfbad9c
D3Pang <derekpang2006@yahoo.com>
2016-12-02 18:46:14 -0800

    setters and toString for Telephone

--- Fulfillment/src/values/Telephone.java
+++ Fulfillment/src/values/Telephone.java
@@ -14,18 +14,37 @@ public class Telephone implements ITelephone {
 		this.extension = extension;
 		this.countryCode = countryCode;
 	}
+	public String toString() {
+		return countryCode+" "+areaCode+" "+number+" "+
+				extension;
+	}
 	
 	public String getAreaCode() {
 		return areaCode;
 	}
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
+	
 	public String getNumber() {
 		return number;
 	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
 	public String getExtension() {
 		return extension;
 	}
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+	
 	public String getCountryCode() {
 		return countryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
 	}
 
 }
